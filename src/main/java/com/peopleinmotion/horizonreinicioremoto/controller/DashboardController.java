44d121494230fe709e4ed8256f147ab653d750a4
avbravo <avbravo@avbravo>
2022-02-20 22:56:04 -0500

    implementa @Cache en los entity para desabilitar el cache

--- src/main/java/com/peopleinmotion/horizonreinicioremoto/controller/DashboardController.java
+++ src/main/java/com/peopleinmotion/horizonreinicioremoto/controller/DashboardController.java
@@ -501,5 +501,20 @@ showDialog=Boolean.TRUE;
         return texto;
     }
 // </editor-fold>
+    
+     public void onIdle() {
+         ConsoleUtil.info("onIdle() "+DateUtil.fechaHoraActual());
+         fillCarouselAccionReciente();
+         JsfUtil.warningMessage("No activity."+ "User is idle");
+//        FacesContext.getCurrentInstance().addMessage(null, new FacesMessage(FacesMessage.SEVERITY_WARN,
+//                "No activity.", "User is idle"));
+    }
+
+    public void onActive() {
+          ConsoleUtil.info("onActive() "+DateUtil.fechaHoraActual());
+                 JsfUtil.warningMessage(     "Activity detected"+ "User is active");
+//        FacesContext.getCurrentInstance().addMessage(null, new FacesMessage(FacesMessage.SEVERITY_WARN,
+//                "Activity detected", "User is active"));
+    }
 
 }
